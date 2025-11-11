148ad4d6d420ea5bf3423f45c3e55d960b865a57
Ankur Sinha <afs6483@psu.edu>
2025-11-11 12:02:38 -0500

    timer shows in log cat

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,60 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+    private lateinit var timerTextView : TextView
+
+     val serviceConnection = object: ServiceConnection{
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+         override fun onServiceDisconnected(p0: ComponentName?) {
+                 isConnected = false
+         }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        timerTextView = findViewById(R.id.textView)
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
+
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
+            //startService(Intent(this, TimerService::class.java))
+            //bindService(Intent(this, TimerService::class.java), serviceConnection, BIND_AUTO_CREATE)
+            if(isConnected) timerBinder.start(100)
 
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
+            if(isConnected) timerBinder.pause()
+
+
 
         }
+    }
+
+    override fun onDestroy() {
+        unbindService(serviceConnection)
+        super.onDestroy()
     }
 }
