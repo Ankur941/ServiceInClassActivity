935b5e1f0e62a25fcef0892d35962a760c5d1337
Ankur Sinha <afs6483@psu.edu>
2025-11-18 12:24:21 -0500

    menu added

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -9,6 +9,8 @@ import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.os.Message
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import kotlin.concurrent.timer
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     var isConnected = false
     private lateinit var timerTextView : TextView
     private var isRunning = false
+    private var startPauseMenuItem: MenuItem? = null
 
     val timerHandler = object: Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message){
@@ -57,29 +60,12 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.startButton).setOnClickListener {
             //startService(Intent(this, TimerService::class.java))
             //bindService(Intent(this, TimerService::class.java), serviceConnection, BIND_AUTO_CREATE)
-            if(isConnected) {
-                if(isRunning){
-                    timerBinder.pause()
-                    startButton.text = "Start"
-
-                }else {
-                    timerBinder.start(100)
-                    startButton.text = "Pause"
-                }
-
-                isRunning = !isRunning
-
-            }
+            handleStartPause()
 
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-            if (isConnected) {
-                timerBinder.stop()
-                isRunning = false
-                startButton.text = "Start"
-                timerTextView.text = "0"
-            }
+            handleStop()
 
            //explicitly stop
 
@@ -88,8 +74,73 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onDestroy() {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        startPauseMenuItem = menu?.findItem(R.id.action_start_pause)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_start_pause -> {
+                handleStartPause()
+                true
+            }
+            R.id.action_stop -> {
+                handleStop()
+                true
+
+            }
+            else -> super.onOptionsItemSelected(item)
+
+
+        }
+    }
+
+    private fun handleStop(){
+        if (isConnected) {
+            timerBinder.stop()
+            isRunning = false
+            updateMenuState()
+            timerTextView.text = "0"
+        }
+    }
+
+    private fun handleStartPause()
+    {
         if(isConnected){
+            isRunning = if(isRunning){
+                timerBinder.pause()
+                false
+
+            }else {
+                timerBinder.start(100)
+                true
+            }
+            updateMenuState()
+        }
+    }
+
+    private fun updateMenuState(){
+        val startButton = findViewById<Button>(R.id.startButton)
+        if(isRunning){
+            startButton.text = getString(R.string.pause)
+            startPauseMenuItem?.title = getString(R.string.pause)
+            startPauseMenuItem?.setIcon(android.R.drawable.ic_media_pause)
+
+        } else {
+            startButton.text = getString(R.string.start)
+            startPauseMenuItem?.title = getString(R.string.start)
+            startPauseMenuItem?.setIcon(android.R.drawable.ic_media_play)
+
+        }
+    }
+
+
+
+    override fun onDestroy() {
+        if(isRunning){
             unbindService(serviceConnection)
             isConnected = false
         }
